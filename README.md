3434756e01f3ff7d29eb6f5d5591f71bc2c3fdfd
BJ <01bbae@gmail.com>
2022-06-07 15:19:55 -0700

    preliminary handling in dbhandler

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # GraphLib
+
+Dataset:
+
+https://www.kaggle.com/datasets/Cornell-University/arxiv
+
+Last Updated on 6/7/2022
--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 from neo4j import GraphDatabase
 from neo4j.exceptions import ServiceUnavailable
+from typing import List
 
 
 class DatabaseHandler:
@@ -9,10 +10,22 @@ class DatabaseHandler:
     def close(self):
         self.driver.close()
 
-    # def test_connection(self):
-    #     # self.driver.
+    def create_author_node(self, name: List[str]):
+        # add person
+        self.run("CREATE (a:Author {name: $name})", name = name)
+
+    def create_paper_node(self, paper: dict[str, str], org):
+        # add paper
+        id = paper["id"]
+        submitter = paper["submitter"]
+        title = paper["title"]
+        comments = paper["comments"]
+        journal_ref = paper["journal-ref"]
+        doi = paper["doi"]
+        report_no = paper["report-no"]
+        catagories = paper["catagories"]
+        self.run("CREATE (a:Paper {paper: $paper, org = $pap})", paper = paper, org = org)
+        for authors in paper["authors_parsed"]:
+            self.create_author_node(authors)
     
-    # def create_author_node(self, name, org):
-    #     # add person
-    #     self.run("CREATE (a:Author {name: $name, org = $org})", name = name, org = org)
-    
+
