60598f1b73609a8d916e7d97a575a5c50bdc5151
BJ Bae <01bbae@gmail.com>
2022-06-10 20:56:29 -0700

    compiled src/*.ts

--- express-server/src/server.js
+++ express-server/src/server.js
@@ -1,3 +1,19 @@
 "use strict";
-const express = require('express');
-console.log("hi");
+exports.__esModule = true;
+var express = require("express");
+var app = express();
+var port = 5000;
+app.listen(port, function () {
+    console.log("listening on ".concat(port));
+});
+app.get('/', function (req, res) {
+    console.log("in /");
+});
+app.get('/query', function (req, res) {
+    console.log("in /query");
+    console.log(req.query);
+    if (typeof req.query["query"] !== "string") {
+        throw new Error("Query is not a string");
+    }
+    var query = req.query["query"];
+});
