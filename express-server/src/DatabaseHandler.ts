0569f1feee4afe441730457eb283174ddd4686cd
BJ <01bbae@gmail.com>
2022-06-17 02:49:22 -0700

    removed repeated secrets.json file

--- express-server/src/DatabaseHandler.ts
+++ express-server/src/DatabaseHandler.ts
@@ -29,7 +29,7 @@ export class DatabaseHandler {
     }
 
     async create_papers_node(this: any, paper: Paper){
-        await this.#session.run('CREATE p:Paper {id: $id, submitter: $submitter, authors: $author, title: $title, comments: $comments, journal_ref: $journal_ref, doi: $doi, report_no: $report_no, catagories: $catagories, license: $license, abstract: $abstract, versions: $versions, update_date: $update_date} RETURN p ', paper);
+        await this.#session.run('MERGE p:Paper {id: $id, submitter: $submitter, authors: $author, title: $title, comments: $comments, journal_ref: $journal_ref, doi: $doi, report_no: $report_no, catagories: $catagories, license: $license, abstract: $abstract, versions: $versions, update_date: $update_date} RETURN p ', paper);
         for (let author in paper.authors_parsed){
             this.create_authors_node(paper,author);
         }
@@ -38,14 +38,14 @@ export class DatabaseHandler {
     }
 
     create_authors_node(paper: Paper, author: Array<string>){
-        this.#session.run('CREATE a:Author {name1: $n1, name2: $n2, name3: $n3} - [:AUTHOR_OF] -> p:Paper RETURN a ', {n1: author[0], n2: author[1], n3: author[2]});
+        this.#session.run('MERGE (a:Author {name1: $n1, name2: $n2, name3: $n3}) - [:AUTHOR_OF] -> (p:Paper) RETURN a ', {n1: author[0], n2: author[1], n3: author[2]});
     }
 
     create_topic_node(paper: Paper, catagory: string){
-        this.#session.run('CREATE c:Catagory {catagory: $topic} - [:Catagorizes] -> p:Paper RETURN c', {topic: catagory});
+        this.#session.run('MERGE (c:Catagory {catagory: $topic}) - [:Catagorizes] -> (p:Paper {}) RETURN c', {topic: catagory});
     }
     
     create_journal_node(paper: Paper, journal_ref:string){
-        this.#session.run('CREATE p:Paper - [:FEATURED_IN] -> j:Journal {journal_ref: $journal} RETURN j', {journal: journal_ref});
+        this.#session.run('MERGE (p:Paper) - [:FEATURED_IN] -> (j:Journal {journal_ref: $journal}) RETURN j', {journal: journal_ref});
     }
 }
